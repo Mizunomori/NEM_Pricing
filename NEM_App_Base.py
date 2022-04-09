2a4539cae47cdeeafb968080ce0e7965382de2c3
Mizunomori <hhh58@cornell.edu>
2022-04-09 17:43:19 -0400

    Inputs

--- NEM_App_Base.py
+++ NEM_App_Base.py
@@ -6,7 +6,19 @@ import streamlit as st
 import numpy as np
 import time 
 import pandas as pd
-
+import geopy
 
 st.title("NEM Pricing Calculator")
-st.subheader("Demonstration of data elements") 
+st.subheader("Inputs for Calculation") 
+
+pv_size = st.text_input('Size of PV Array (kW)', '500')
+st.write('The current array size is', pv_size, 'kW') 
+
+
+state = zip_code = st.text_input('State of Residency', 'NY')
+city= st.text_input('City of Residency', 'Ithaca')
+
+zipcode = st.text_input('Zipcode', '14850')
+
+place = state + city + zipcode
+st.write('The location being queried is', place) 
