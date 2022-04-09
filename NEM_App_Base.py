90713f7eb46d830640fc29851f01494ac524a29e
Mizunomori <hhh58@cornell.edu>
2022-04-09 15:35:49 -0400

    title

---
+++ NEM_App_Base.py
@@ -0,0 +1,12 @@
+from collections import namedtuple
+import altair as alt
+#from tkinter import Image
+import math
+import streamlit as st
+import numpy as np
+import time 
+import pandas as pd
+
+
+st.title("NEM Pricing Calculator")
+st.subheader("Demonstration of data elements") 
