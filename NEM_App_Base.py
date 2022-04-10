f4dde4cdc47eadfab095657e3bcf2b86e71ef6a2
Mizunomori <hhh58@cornell.edu>
2022-04-09 22:50:51 -0400

    json

--- NEM_App_Base.py
+++ NEM_App_Base.py
@@ -7,7 +7,8 @@ import numpy as np
 import time 
 import pandas as pd
 from geopy.geocoders import Nominatim 
-
+import requests 
+import json
 
 st.title("NEM Pricing Calculator")
 st.subheader("Inputs for Calculation") 
@@ -28,8 +29,16 @@ geolocator = Nominatim(user_agent="geoapiExercises")
 
 location = geolocator.geocode(place) 
 coords = [str(location.latitude), str(location.longitude)]
+st.write('Your Latitude and Longitude is: (' + coords[0]+ ', ' +coords[1] + ')') 
 
+# Now Use the Latitude and Longitude Given to doan API pull of the soalr data from NREL
 api_pull = 'https://developer.nrel.gov/api/solar/solar_resource/v1.xml?lat=' + coords[0]+ '&lon='+ coords[1] + '&api_key=90IdyNRwQOO0iv3PXV6wPAbfHl8dKrBFXWDWBadf' 
 
 
-st.write('Your Latitude and Longitude is: (' + coords[0]+ ', ' +coords[1]) 
+
+response_API = requests.get(api_pull) 
+
+data =response_API.text
+parse_json = pd.DataFrame(json.loads(data))
+
+st.dataframe(parse_json)
