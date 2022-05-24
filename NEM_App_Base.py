8c7403b7d5256d0a3e411d281591ec8b1c57f2c7
Mizunomori <hhh58@cornell.edu>
2022-05-24 18:58:28 -0400

    Whjole new world

--- NEM_App_Base.py
+++ NEM_App_Base.py
@@ -97,12 +97,12 @@ Pd2 = Pdict['outputs']
 price_df = pd.DataFrame.from_dict(Pd2) 
 res_price = float(price_df.residential)
 
-e_bill = float(st.text_input('What is your average electricity bill ($)?', '100'))
+e_bill = float(st.text_input('What is your average electricity bill in the summer ($)?', '100'))
 st.write('Price of electricity for a residential consumer in your area is $ %2.3f/kWh' %res_price)
 
-
+# Calcualting monthly and daily energy usage
 e_load = e_bill/res_price
-
+e_daily = e_load/31
 
 st.header('Results')
 
@@ -134,33 +134,24 @@ NEM = 'NEM 2.0'
 cost = np.zeros(13)
 solar_prod = np.zeros(13) 
 savings = np.zeros(13)
-if NEM == 'NEM 2.0':
-#NEM 2.0 
+
      
-     solar_prod[:12] = solar_df.ac_monthly
-     solar_prod[12] = np.sum(solar_prod[:12])
-     demand = e_load
-     for i in range(0,12): 
+solar_prod[:12] = solar_df.ac_monthly
+solar_prod[12] = np.sum(solar_prod[:12])
+demand = e_load
+for i in range(0,12): 
           
-          if e_load >= solar_prod[i]:
-               Ppi = res_price
-               savings[i] = Ppi * solar_prod[i]
-               cost[i] = Ppi * demand - savings[i]
-               
-          else: 
-               Ppi = res_price -0.03
-               savings[i] = Ppi * solar_prod[i]
-               cost[i] = Ppi * demand - savings[i]
-elif NEM == 'NEM 1.0':
-     #NEM 1.0 
-     for i in range(0,12): 
-          
-          if e_load >= solar_prod[i]:
-               Ppi = res_price
-               cost = Ppi * (demand[i] - solar_prod[i])
-          else: 
-               Ppi = res_price
-               cost = Ppi * (demand[i] -solar_prod[i]) 
+     if e_load >= solar_prod[i]:
+          Ppi = res_price
+          savings[i] = Ppi * solar_prod[i]
+          cost[i] = Ppi * demand - savings[i]
+           
+     else: 
+          Ppi = res_price -0.03
+          savings[i] = Ppi * solar_prod[i]
+          cost[i] = Ppi * demand - savings[i]
+
+
 
 cost[12] = np.sum(cost[:12])
 savings[12] = np.sum(savings)
@@ -188,16 +179,105 @@ obj = TimezoneFinder()
 tz = obj.timezone_at(lng = lon, lat = lat) 
 site_location = loc.Location(lat, lon, tz=tz)
 
-dates= ['01-01-2019', '01-01-2021']
-days = [31, 28, 31, 30, 31, 30, 31,31, 30, 31, 30, 31] 
+dates= ['07-01-2019', '07-01-2021','10-01-2019', '10-01-2021']
+days = 31
 
-dy_irad = np.zeros([2,8760,2])
-#for i in range(0,len(dates)):
-#dy_irad[i,:,:] = get_irradiance(site_location, dates[i], tilt, azimuth)
-     
+dy_irad = np.zeros([4,744,2])
+for i in range(0,len(dates)):
+     dy_irad[i,:,:] = get_irradiance(site_location, dates[i], tilt, azimuth)
+ 
    
-#for h in range(0,len(days)): 
-#avg_irad = (dy_irad[0,:,:] + dy_irad[1,:,:])/2 
+
+summ_irad = (dy_irad[0,:,:] + dy_irad[1,:,:])/2 
+fall_irad = (dy_irad[2,:,:] + dy_irad[3,:,:])/2 
 
 
-#for j in range(0, len(days)):
+summ_hrs = np.zeros([24])
+fall_hrs = np.zeros([24])
+for j in range(0, 31):
+     summ_hrs[:] = summ_hrs[:]+ summ_irad[j*24:(j*24+24),1]/31
+     fall_hrs[:] = fall_hrs[:] + fall_irad[j*24:(j*24+24),1]/31 
+
+summ_pwr = summ_hrs *.2
+fall_pwr = fall_hrs * .2
+
+summ = [0.030123129,0.028412052,	0.027893306,0.0282233,0.029348275,0.028643837 \
+     ,0.034073772,0.038129946,0.040214751,0.040616718,0.043581589,0.046959892,0.046221516,0.047289287 \
+          ,0.049114413,0.051597397,0.05465336,0.054475666,0.051541176,0.054747908,0.053664921,0.04792486,0.039092799,0.033456129] * e_daily
+
+fall = [0.027909024,	0.028068565,	0.028688672,	0.034958594,	0.036082744,	0.036810749,	0.048275996,	0.045167662,	0.042198906,\
+     	0.042714116,	0.042488399,	0.044230298,	0.042157561,	0.04218743,	0.040286818,	0.041876612,	0.046910151,	0.057671775,	0.055978038,\
+               	0.056256477,	0.051948516,	0.043169701,	0.03390215,	0.030061045] *e_daily * .9
+
+pm = res_price - 0.03 
+pp = res_price
+
+q_s = np.zeros([24])
+q_f = np.zeros([24])
+for h in range(0,23): 
+     a_s = summ[h] + res_price
+     a_f = fall[h] + res_price 
+     qp_s = np.max([summ_pwr[h] +.01, a_s])
+     qm_s = summ_pwr[h] - .05 
+     qp_f = np.max([fall_pwr[h] +.01, a_f])
+     qm_f = fall_pwr[h] - .05
+     #calculations for summer  
+     if summ_pwr[h]   != 0:
+          while a_s*(qp_s+.05) - .5 * (qp_s+.05)**2 -pp*(qp_s+.05 - summ_pwr[h]) > a_s*(qp_s) - .5 * (qp_s)**2 -pp*(qp_s - summ_pwr[h]):
+               qp_s = qp_s +.05
+          while a_s*(qm_s-.05) - .5 * (qm_s-.05)**2 -pm*(qm_s+.05 - summ_pwr[h]) > a_s*(qm_s) - .5 * (qm_s)**2 -pm*(qm_s - summ_pwr[h]):
+               qm_s = qm_s -.05
+     else:
+          while a_s*(qp_s+.05) - .5 * (qp_s+.05)**2 -pp*(qp_s+.05 - summ_pwr[h]) > a_s*(qp_s) - .5 * (qp_s)**2 -pp*(qp_s - summ_pwr[h]):
+               qp_s = qp_s +.05
+     Sps = a_s*(qp_s) - .5 * (qp_s)**2 -pp*(qp_s - summ_pwr[h])
+     Sms = a_s*(qm_s) - .5 * (qm_s)**2 -pm*(qm_s - summ_pwr[h])
+     Ses = a_s * summ_pwr[h] - .5*(summ_pwr[h]**2) 
+     Ss = np.max([Sps,Sms,Ses])
+     if Ss == Sps : 
+          q_s[h] = qp_s 
+     elif Ss == Sms: 
+          q_s[h] = qm_s 
+     else: 
+          q_s[h] = summ_pwr[h] 
+
+     #Calculations for fall
+     if fall_pwr[h]   != 0:
+          while a_s*(qp_f+.05) - .5 * (qp_f+.05)**2 -pp*(qp_f+.05 - fall_pwr[h]) > a_s*(qp_f) - .5 * (qp_f)**2 -pp*(qp_f - fall_pwr[h]):
+               qp_f = qp_f +.05
+          while a_s*(qm_f-.05) - .5 * (qm_f-.05)**2 -pm*(qm_f+.05 - fall_pwr[h]) > a_s*(qm_f) - .5 * (qm_f)**2 -pm*(qm_f - fall_pwr[h]):
+               qm_f = qm_f -.05
+     else:
+          while a_s*(qp_f+.05) - .5 * (qp_f+.05)**2 -pp*(qp_f+.05 - fall_pwr[h]) > a_s*(qp_f) - .5 * (qp_f)**2 -pp*(qp_f - fall_pwr[h]):
+               qp_f = qp_f +.05 
+     
+     Spf = a_f*(qp_f) - .5 * (qp_f)**2 -pp*(qp_f - fall_pwr[h])
+     Smf = a_f*(qm_f) - .5 * (qm_f)**2 -pm*(qm_f - fall_pwr[h])
+     Sef = a_f * fall_pwr[h] - .5*(fall_pwr[h]**2) 
+     Sf = np.max([Spf,Smf,Sef])
+     if Sf == Spf : 
+          q_f[h] = qp_f 
+     elif Sf == Smf: 
+          q_f[h] = qm_f 
+     else: 
+          q_f[h] = fall_pwr[h] 
+
+fig,axs = plt.subplots(1,1, figsize =(8,6))
+
+hours = ['12 AM',	'1 AM',	'2 AM',	'3 AM',	'4 AM',	'5 AM',	'6 AM',	'7AM',	'8 AM',	'9 AM', \
+     	'10 AM',	'11 AM',	'12 PM',	'1 PM',	'2 PM',	'3 PM',	'4 PM',	'5 PM',	'6 PM',	'7 PM',	'8 PM',	'9 PM'	'10 PM'	'11 PM' '12 PM']
+
+axs.plot(hours, summ, '--', label = 'Summer Consumption Before')
+axs.plot(hours, fall, '--', label = 'Fall Consumption Before') 
+axs.plot(hours, q_s, '-', label = 'Summer Consumption w/ Solar') 
+axs.plot(hours, q_f, '-', label = 'Fall Consumption w/ Solar')
+axs.set_xlabel('Hour')
+axs.set_ylabel('Demand (kWh')
+axs.set_title('Demand vs. Hour') 
+axs.legend()
+plt.rcParams['font.size'] = 13
+st.pyplot(fig) 
+
+hr_df = pd.DataFrame({'Hours': hours, 'Summer Before (kWh)' : summ, 'Summer w/ Solar (kWh)': q_s, 'Fall Before (kWh)': fall, 'Fall w/ Solar (kWh)': q_f})
+
+st.dataframe(hr_df)
